f5819bd58ff2ba24e7770d6e333e121e418b4ecc
nathanbc777 <nathan.castanha@escola.pr.gov.br>
2026-05-27 22:01:42 -0300

    twepo

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + "minutos" + segundos + " segundos.";
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos.";
 
     
 }
