9348120c04379606cd02b0c30bb81f54b9092f43
nathanbc777 <nathan.castanha@escola.pr.gov.br>
2026-05-28 21:20:03 -0300

    28/05

--- main.js
+++ main.js
@@ -40,6 +40,11 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizarCronometro() {
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
     for (let i=0; i < contadores.length; i++) {
         contadores[i].textContent = calculaTempo(tempos[i]);
     }
