922d558d8e5639aa9425f98dacb13d1cfbd2e2ba
nathanbc777 <nathan.castanha@escola.pr.gov.br>
2026-06-30 21:10:30 -0300

    aula 30

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ const tempoObjetivo4 = new Date("2026-12-11T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+const situacoes = document.querySelectorAll(".situacao");
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -36,6 +38,31 @@ function calculaTempo(tempoObjetivo) {
         return [dias,horas,minutos,segundos];
     } else {
         return [0,0,0,0];
+    }
+}
+
+funtion calculaSituacao(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    if (tempoFinal > 0) {
+        return "Ainda há tempo!";
+    } else {
+        return "Perdeu !!!"
+    }
+}
+
+function atualizaSituacao() {
+    for( let i=0; i < situacao.length; i++) {
+        situacoes[i].textContent = calculaSituacao(tempos[i]);
     }
 }
 
@@ -56,3 +83,4 @@ function comecaCronometro() {
 }
 
 comecaCronometro();
+atualizaSituacao();
